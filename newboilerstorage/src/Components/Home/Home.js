126d05f43fe67ba9388f30df7794c3cf396a5dfb
jimmydinh211 <minhdinhtranhoang@gmail.com>
2023-05-30 00:20:10 +0700

    Center homepage nav

--- newboilerstorage/src/Components/Home/Home.js
+++ newboilerstorage/src/Components/Home/Home.js
@@ -384,41 +384,23 @@ function Hero() {
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          className="mx-auto flex max-w-7xl items-center justify-center p-6 lg:px-8"
           aria-label="Global"
-        >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Emblify AI</span>
-              <img
-                className="h-10 w-auto"
-                src={("./images/logo.png")}
-                alt=""
-              />
-            </a>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
+        > 
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                {item.name}
-              </a>
-            ))}
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  {item.name}
+                </a>
+              ))}
           </div>
+          
         </nav>
+        
         <Dialog
           as="div"
           className="lg:hidden"
